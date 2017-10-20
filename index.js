b0ac417b027774ce106a78396dac61aa1a14245a
Narayan Waraich <narayanwaraich@gmail.com>
2017-10-21 04:48:47 +0530

    breakout game....in lingo

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var canvas = document.getElementById('canvas');
+canvas.width  = document.documentElement.clientWidth;
+canvas.height = document.documentElement.clientHeight;
 var ctx = canvas.getContext('2d');
 var raf, i ;
 var lines = finishedLines = [];
-var steps = 10; //  10 pixel per frame
+var steps = 20; //  10 pixel per frame
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -22,18 +24,18 @@ function Line(x1, y1, lineLength, direction) {
     this.draw = function() {
       if (this.step === steps) {
         this.finished = true;
-        return false;
+        //return false;
       }
       ctx.beginPath();
       if (this.direction === 'vertical'){
-        ctx.moveTo(this.x1, (this.y1+((this.lineLength / steps)*this.step)-1));
+        ctx.moveTo(this.x1, this.y1);
         ctx.lineTo(this.x1, (this.y1+((this.lineLength / steps)*(this.step+1))));
       } else {
-        ctx.moveTo((this.x1+((this.lineLength / steps)*this.step)), this.y1);
+        ctx.moveTo(this.x1, this.y1);
         ctx.lineTo((this.x1+((this.lineLength / steps)*(this.step+1))), this.y1);
       }
       ctx.stroke();
-      this.step++;
+      if(!this.finished) this.step++;
       return true;
     }
 }
@@ -42,9 +44,9 @@ function Line(x1, y1, lineLength, direction) {
 function draw(ts) {
   //console.log(ts);
   ctx.lineWidth = 1;
-  ctx.strokeStyle = 'red';
+  ctx.strokeStyle = 'rgba(0,0,128,1)';
   ctx.lineCap = 'butt';
-  //ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   i++;
   let x1 = getRandomInt(0, canvas.width);
   let y1 = getRandomInt(0, canvas.height);
@@ -52,21 +54,17 @@ function draw(ts) {
   let direction = (i%2===0) ? 'horizontal' : 'vertical' ;
   lines.push(new Line(x1, y1, lineLength, direction));
   for (var j = 0; j < lines.length; j++) {
-    let animStatus = lines[j].draw();
-    if(!animStatus && (j > -1)){
-      finishedLines.push(lines[j]);
-      lines.splice(j, 1);
-    }
+    lines[j].draw();
   }
-  if(i < 50){
+  if (i<1000) {
     window.requestAnimationFrame(draw);
   }
 }
 
 function animate(){
   //ctx.clearRect(0, 0, canvas.width, canvas.width); // clear canvas
-//  ctx.translate(canvas.width/2,0);
-//  ctx.rotate(Math.PI/4);
+  ctx.translate(canvas.width/2,0);
+  ctx.rotate(Math.PI/4);
   i = 0 ;
   draw();
 //  raf = window.requestAnimationFrame(draw);
